996c19201e731ee514bdcfa8b9dc18005f715e56
Malte Andersson <malte.andersson@elev.ga.ntig.se>
2026-01-27 10:06:39 +0100

    lek2 övn4 klar

--- Lek2 Övning 4/src/App.java
+++ Lek2 Övning 4/src/App.java
@@ -8,6 +8,11 @@ public class App {
         //Lägg till personer
         personer.add(new Person("Bert",  19740302));
         personer.add(new Person("Greg", 19850504));
+        personer.add(new Person("Roger", 19880808));
+        personer.add(new Person("Lars", 20050405));
+        personer.add(new Person("Larsa", 20070405));
+
+
         IO.println(personer);
 
         for(Person p: personer){
@@ -17,5 +22,17 @@ public class App {
                 IO.println("Miss");
             }
         }
+
+        personer.remove(0);
+        IO.println(personer + " är kvar");
+
+        for(Person p: personer){
+            if (p.getPersonnummer()==19880808) {
+                IO.println(p + " tas bort");
+                personer.remove(p);
+            }
+        }
+        
+        IO.println(personer.size() + " stycken kvar");
     }
 }
