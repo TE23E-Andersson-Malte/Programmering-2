7e427ecceff73ea56db04e82c2f506e30b57a4b5
Malte Andersson <malte.andersson@elev.ga.ntig.se>
2026-03-18 14:41:09 +0100

    klar med fortsättning (räkna ut vinsten) på uppgift 3 lek 5

--- Lek5 Uppgift 3/src/App.java
+++ Lek5 Uppgift 3/src/App.java
@@ -12,6 +12,8 @@ public class App {
         sortiment.put("glass", new Vara("Glass", 10.0, 3));
         sortiment.put("godispåse", new Vara("Godispåse", 10.0, 3));
 
+        double vinst = 0;
+
         while (true) {
             IO.print("""
                     \n---KIOSK---
@@ -36,14 +38,19 @@ public class App {
                     if (vara != null) {
                         if (vara.sälj()) {
                             IO.println("\nSålde: " + sortiment.get(varanamn));
+                            vinst += vara.getPris();
+                            IO.println("Vinst efter försäljning: " + vinst + "kr");
                         } else {
                             IO.print("\nSlut i lager. Vill du köpa in fler? (ja/nej): ");
                             String svar = IO.readln();
                             if (svar.equals("ja")) {
-                                IO.println("Hur många?");
+                                IO.print("Hur många?");
                                 int antal = Integer.parseInt(IO.readln());
                                 vara.köp_in(antal);
-                                IO.println("Nytt lager: " + vara.getAntal());
+                                IO.println("\nNytt lager: " + vara.getAntal());
+
+                                vinst -= antal * vara.getPris() / 2;
+                                IO.println("Vinst efter inköp: " + vinst + "kr");
                             }
                         }
                     } else{
@@ -51,7 +58,8 @@ public class App {
                     }
                     break;
                 case 3:
-                    break;
+                    IO.println("\nLämnar kiosken...");
+                    return;
                 default:
                     break;
             }
